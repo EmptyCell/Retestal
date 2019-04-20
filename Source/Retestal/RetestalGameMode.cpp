c9bd7173f286a44f6ca441b8e22437d61586f309
Unknown <graiiner@gmail.com>
2019-04-21 01:40:20 +0200

    Project-wide refactor
    Refactor of whole project:
    * NPC folder
    * Weapons folder
      - RetestalProjectile to BallProjectile.
    * Player folder
      - RetestalCharacter to FirstPersonCharacter.
    * Other additional changes

--- Source/Retestal/RetestalGameMode.cpp
+++ Source/Retestal/RetestalGameMode.cpp
@@ -2,14 +2,14 @@
 
 #include "RetestalGameMode.h"
 #include "RetestalHUD.h"
-#include "RetestalCharacter.h"
+#include "Player/FirstPersonCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 ARetestalGameMode::ARetestalGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behaviour/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
